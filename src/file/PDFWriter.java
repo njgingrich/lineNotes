3cc649c1e72fe771302a5b0deb93a88232acb85e
Nathan <njgingrich@gmail.com>
2014-10-28 03:56:28 -0400

    Table layout works excluding bolding
    Bolding of error within line does not work.

--- src/file/PDFWriter.java
+++ src/file/PDFWriter.java
@@ -19,9 +19,12 @@ package file;
 import com.itextpdf.text.Chunk;
 import com.itextpdf.text.Document;
 import com.itextpdf.text.DocumentException;
+import com.itextpdf.text.Element;
 import com.itextpdf.text.Font;
 import com.itextpdf.text.Font.FontFamily;
 import com.itextpdf.text.Paragraph;
+import com.itextpdf.text.pdf.PdfPCell;
+import com.itextpdf.text.pdf.PdfPTable;
 import com.itextpdf.text.pdf.PdfWriter;
 import java.io.File;
 import java.io.FileNotFoundException;
@@ -50,11 +53,17 @@ public class PDFWriter {
 
         document.open();
         Font lineFont = new Font(FontFamily.TIMES_ROMAN);
-        lineFont.setStyle(Font.UNDERLINE);
+        Font lineFontUnderline = new Font(FontFamily.TIMES_ROMAN);
+        lineFontUnderline.setStyle(Font.UNDERLINE);
         Font errorFont = new Font(FontFamily.TIMES_ROMAN);
         errorFont.setStyle(Font.ITALIC);
+        Font errorFontBold = new Font(FontFamily.TIMES_ROMAN);
+        errorFontBold.setStyle(Font.BOLDITALIC);
         
-        Paragraph header = new Paragraph();
+        /*
+        Trying it the normal way
+        */
+        /*Paragraph header = new Paragraph();
         Chunk dateTitle = new Chunk("Date: ");
         Chunk date = new Chunk(" 10/27 ");
         date.setFont(lineFont);
@@ -72,7 +81,7 @@ public class PDFWriter {
         document.add(Chunk.NEWLINE);
         
         for (LineNote note : role.getNotes()) {
-            Paragraph line = new Paragraph("Line: ", lineFont);
+            Paragraph line = new Paragraph("Page: " + note.getPageNum() + "      Line: ", lineFont);
             line.add(note.getLine());
             document.add(line);
             document.add(Chunk.NEWLINE);
@@ -91,7 +100,65 @@ public class PDFWriter {
             }
             document.add(Chunk.NEWLINE);
             document.add(Chunk.NEWLINE);
+        }*/
+        
+        /*
+        Trying to use a table so the layout works better
+        */
+        PdfPTable headerTable = new PdfPTable(2);
+        headerTable.setWidthPercentage(100);
+        PdfPCell dateCell = new PdfPCell(new Paragraph("Date: 10/27", lineFont));
+        dateCell.setHorizontalAlignment(Element.ALIGN_LEFT);
+        dateCell.setBorder(PdfPCell.NO_BORDER);
+        dateCell.setPadding(0.3f);
+        PdfPCell characterCell = new PdfPCell(new Paragraph("Character: " + role.getName(), lineFont));
+        characterCell.setHorizontalAlignment(Element.ALIGN_RIGHT);
+        characterCell.setBorder(PdfPCell.NO_BORDER);
+        
+        headerTable.addCell(dateCell);
+        headerTable.addCell(characterCell);
+        document.add(headerTable);
+        document.add(Chunk.NEWLINE);
+        document.add(Chunk.NEWLINE);
+        
+        
+        for (LineNote note : role.getNotes()) {
+            PdfPTable lineTable = new PdfPTable(2);
+            PdfPCell pageCell = new PdfPCell(new Paragraph("Page: " + note.getPageNum(), lineFont));
+            pageCell.setPadding(20);
+            pageCell.setPaddingLeft(0);
+            pageCell.setBorder(PdfPCell.NO_BORDER);
+            PdfPCell lineCell = new PdfPCell(new Paragraph("Line: " + note.getLine(), lineFontUnderline));
+            lineCell.setPadding(20);
+            lineCell.setBorder(PdfPCell.NO_BORDER);
+            lineTable.setWidthPercentage(100);
+            lineTable.setWidths(new int[] {2, 10});
+            lineTable.setSpacingBefore(1f);
+            lineTable.setSpacingAfter(1f);
+            
+            lineTable.addCell(pageCell);
+            lineTable.addCell(lineCell);
+            document.add(lineTable);
+            document.add(Chunk.NEWLINE);
+
+            
+            PdfPTable errorTable = new PdfPTable(7);
+            errorTable.setWidthPercentage(100);
+            for (int i = 0; i < chunks.length; i++) {
+                errorFont.setStyle(Font.ITALIC);
+                PdfPCell cell;
+                if (i == errorIX) {
+                    System.out.println(chunks[i]);
+                    cell = new PdfPCell(new Paragraph(chunks[i], errorFontBold));
+                } else {
+                    cell = new PdfPCell(new Paragraph(chunks[i], errorFont));
+                }
+                cell.setBorder(PdfPCell.NO_BORDER);
+                errorTable.addCell(cell);
+            }
+            document.add(errorTable);
         }
+        
         document.close();
     }
 }
